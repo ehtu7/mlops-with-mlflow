f7a179430ef917b371ffacb14ce05ae16ef587bb
ehtu7 <er.ehtashamazhar7@gmail.com>
2025-07-16 10:57:55 +0530

    first ML Ops integration

---
+++ src/file1.py
@@ -0,0 +1,49 @@
+import mlflow
+import mlflow.sklearn
+from sklearn.datasets import load_wine
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import accuracy_score
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+wine = load_wine()
+X = wine.data
+y = wine.target
+
+# Train test split
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.10, random_state=42)
+
+# Define the params for RF model
+max_depth = 10
+n_estimators = 5
+
+# Mention your experiment below
+# mlflow.set_experiment('YT-MLOPS-Exp1')
+
+
+
+with mlflow.start_run():
+    rf = RandomForestClassifier(max_depth=max_depth, n_estimators=n_estimators)
+    rf.fit(X_train, y_train)
+    y_pred = rf.predict(X_test)
+    acc = accuracy_score(y_test, y_pred)
+
+    # Log metrics
+    mlflow.log_metric("accuracy", acc)
+    mlflow.log_param("max_depth", max_depth)
+    mlflow.log_param("n_estimators", n_estimators)
+
+    # # Compute and plot confusion matrix
+    # from sklearn.metrics import confusion_matrix
+    # cm = confusion_matrix(y_test, y_pred)
+    # plt.figure(figsize=(10, 7))
+    # sns.heatmap(cm, annot=True, fmt="d")
+    # plt.xlabel("Predicted")
+    # plt.ylabel("Actual")
+    # plt.title("Confusion Matrix")
+    # plt.savefig("confusion_matrix.png")
+    # plt.close()
+    # # Log confusion matrix artifact
+    # mlflow.log_artifact("confusion_matrix.png")
+    print("Accuracy:", acc)
